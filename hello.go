28debc5bf9f937608c09a35fa25311f3b8484689
Alec <rcsmith.alec@gmail.com>
2021-06-21 12:29:16 +0100

    Created REST Service with endpoints for all tickets and one ticket by id

--- hello.go
+++ hello.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "net/http"
+	"encoding/json"
+	"strconv"
+	"github.com/gorilla/mux"		// import package using "go get github.com/gorilla/mux"
+)
 
+// Ticket - Our struct for all tickets, to be replaced with DB access
+type Ticket struct {
+    Id      int    `json:"Id"`
+    Title   string `json:"Title"`
+    Desc    string `json:"desc"`
+    Priority int `json:"priority"`
+}
+
+var Tickets []Ticket
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func allTickets(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Tickets)
+	fmt.Println("Endpoint Hit: tickets")
+}
+
+func getTicket(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)	// gets path variables from request
+	id, err := strconv.Atoi(vars["id"])		// accessed using tuple and converted to int
+	if err != nil {
+		panic(err)
+	}
+
+	found := false
+	for _, ticket := range Tickets {
+		if ticket.Id == id {
+			found = true
+			json.NewEncoder(w).Encode(ticket)
+		}
+	}
+
+	if !found {
+		fmt.Fprintf(w, "No Ticket found for id %d!", id)
+	}
+}
+
+func handleRequests() {
+	// creates a new instance of a mux router
+    router := mux.NewRouter().StrictSlash(true)
+
+    router.HandleFunc("/", homePage)
+	router.HandleFunc("/tickets", allTickets)
+	router.HandleFunc("/ticket/{id}", getTicket)
+
+    log.Fatal(http.ListenAndServe(":10000", router))
+}
+
+/*
+	Created using tutorial: https://tutorialedge.net/golang/creating-restful-api-with-golang/
+*/
 func main() {
-	fmt.Println("Hello, World!")
+	// Test Data
+	Tickets = []Ticket{
+        Ticket{Id: 1, Title: "Hello", Desc: "Ticket Description", Priority: 1},
+        Ticket{Id: 2, Title: "Hello 2", Desc: "Ticket Description", Priority: 2},
+    }
+
+	log.Println("Server started on port :10000...")
+    handleRequests()
 }
